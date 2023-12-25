a28e9fa7d102bfdd6cd8168c8939ca38d0399dc4
jaison-x <jaisonveneri@gmail.com>
2023-12-25 18:48:25 -0300

    refactor: change the message ids cache in chatwoot to use a in memory cache
    Remove use of disc cache for optimize performance.
    To make this, we need change to use only one instance of ChatwootService in entire application.

--- src/whatsapp/services/whatsapp.service.ts
+++ src/whatsapp/services/whatsapp.service.ts
@@ -75,6 +75,7 @@ import { getIO } from '../../libs/socket.server';
 import { getSQS, removeQueues as removeQueuesSQS } from '../../libs/sqs.server';
 import { useMultiFileAuthStateDb } from '../../utils/use-multi-file-auth-state-db';
 import { useMultiFileAuthStateRedisDb } from '../../utils/use-multi-file-auth-state-redis-db';
+import { instanceController } from '../../whatsapp/whatsapp.module';
 import {
   ArchiveChatDto,
   DeleteMessage,
@@ -134,7 +135,6 @@ import { Events, MessageSubtype, TypeMediaMessage, wa } from '../types/wa.types'
 import { waMonitor } from '../whatsapp.module';
 import { ChamaaiService } from './chamaai.service';
 import { ChatnodeService } from './chatnode.service';
-import { ChatwootService } from './chatwoot.service';
 import { TypebotService } from './typebot.service';
 
 const retryCache = {};
@@ -173,7 +173,7 @@ export class WAStartupService {
 
   private phoneNumber: string;
 
-  private chatwootService = new ChatwootService(waMonitor, this.configService, this.repository);
+  private chatwootService = instanceController.getChatwootService();
 
   private chatnodeService = new ChatnodeService(waMonitor, this.repository, this.configService, this.chatwootService);
 
