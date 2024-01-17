174c4b5ce6bbf6594a02359bdfc5e1b121e2952c
jaison-x <jaisonveneri@gmail.com>
2024-01-17 17:50:31 -0300

    fix: message 'connection successfully' spamming

--- src/whatsapp/services/chatwoot.service.ts
+++ src/whatsapp/services/chatwoot.service.ts
@@ -2011,6 +2011,7 @@ export class ChatwootService {
             const msgConnection = `🚀 Connection successfully established!`;
             this.logger.verbose('send message to chatwoot');
             await this.createBotMessage(instance, msgConnection, 'incoming');
+            this.waMonitor.waInstances[instance.instanceName].qrCode.count = 0;
           }
         }
       }
